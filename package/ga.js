1599b8d62ad20846281a76cd64c77b48cdc55396
NeverBehave <gayhub@never.pet>
2020-05-22 18:20:59 -0400

    ref: optimize.js

---
+++ package/ga.js
@@ -0,0 +1,3 @@
+const window = require("global/window")
+
+window.ga_api = 'https://async-task.never.workers.dev/optical/blog'
--- saber-node.js
+++ saber-node.js
@@ -1,10 +1,10 @@
 exports.chainWebpack = function(chain) {
-    chain.module
-      .rule('pdf')
-      .test(/\.pdf$/)
-      .use('pdf')
-        .loader('file-loader')
-        .options({
-                name: '[name].[ext]'
-        })
-  }
+  chain.module
+    .rule("pdf")
+    .test(/\.pdf$/)
+    .use("pdf")
+    .loader("file-loader")
+    .options({
+      name: "[name].[ext]",
+    });
+};
